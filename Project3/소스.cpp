5ab51ac15c0f395719c93229152c7f81fe287bc9
KIM LAIM <k5115698@naver.com>
2024-08-01 17:09:24 +0900

    test

--- Project3/소스.cpp
+++ Project3/소스.cpp
@@ -1,6 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS //scanf 오류날 때!
 #include <stdio.h>
 
 void main()
 {
-	int a = 1, b = 2;
+	int a;
+
+	printf("정수를 입력하세요 : ");
+	scanf("%d", &a);
+
+	if ((a % 2) == 0)
+	{
+		printf("짝수를 입력했군요.. \n");
+	}
+	else
+	{
+		printf("홀수를 입력했군요.. \n");
+	}
 }
